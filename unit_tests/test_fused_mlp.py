87edff824e097025640c6dd78703ac428e9d0c6b
kkbwilldo <kimkyu1515@naver.com>
2024-07-08 15:16:27 +0000

    [update] add logs for debugging

--- unit_tests/test_fused_mlp.py
+++ unit_tests/test_fused_mlp.py
@@ -46,20 +46,40 @@ class TestFusedMLP(unittest.TestCase):
 
     @parameterized.expand(itertools.product([1], [8], [256], _FLOAT_TYPES_))
     def test_fused_mlp(self, batch_size, seq_len, block_size, dtype):
-        input_tensor = self.generate_input(batch_size, seq_len, dtype)
+        input_tensor = self.generate_input(batch_size, seq_len, dtype).contiguous()
         self.layer.to(dtype)
 
         with torch.no_grad():
-            torch_output = self.layer(input_tensor)
+#             torch_output = self.layer(input_tensor)
+            gate_proj = self.layer.gate_proj(input_tensor)
+            up_proj = self.layer.up_proj(input_tensor)
+            gate_proj_activated = self.layer.act_fn(gate_proj)
+            intermediate = gate_proj_activated * up_proj
+            torch_output = self.layer.down_proj(intermediate)
 
-        fused_mlp_output = kbkim_kernels.fused_mlp(
+        fused_mlp_output, fused_gate_proj, fused_up_proj, fused_intermediate = kbkim_kernels.fused_mlp(
             input_tensor,
-            self.layer.gate_proj.weight,
-            self.layer.up_proj.weight,
-            self.layer.down_proj.weight,
+            self.layer.gate_proj.weight.contiguous(),
+            self.layer.up_proj.weight.contiguous(),
+            self.layer.down_proj.weight.contiguous(),
             block_size
         )
+        print(f"gate_proj weight shape: {self.layer.gate_proj.weight.shape}")
+
+        print(f"gate_proj shape: {gate_proj.shape}")
+        print(f"fused_gate_proj shape: {fused_gate_proj.shape}")
+        print(f"up_proj shape: {up_proj.shape}")
+        print(f"fused_up_proj shape: {fused_up_proj.shape}")
+        print(f"down_proj shape: {torch_output.shape}")
+        print(f"fused_down_proj shape: {fused_mlp_output.shape}")
+        print(f"intermediate shape: {intermediate.shape}")
+        print(f"fused_intermediate shape: {fused_intermediate.shape}")
         
+        print(f"Max difference in gate_proj: {(gate_proj - fused_gate_proj).abs().max().item()}")
+        print(f"Max difference in up_proj: {(up_proj - fused_up_proj).abs().max().item()}")
+        print(f"Max difference in intermediate: {(intermediate - fused_intermediate).abs().max().item()}")
+        print(f"Max difference in final output: {(torch_output - fused_mlp_output).abs().max().item()}")
+
         self.assertTrue(
             torch.allclose(torch_output, fused_mlp_output, atol=1e-5),
             f"The output is not close for batch_size={batch_size}, seq_len={seq_len}, block_size={block_size} dtype={dtype}"
